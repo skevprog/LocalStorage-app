1a5b53cfbf51825df686fc0550af76e90c9b9c85
skevprog <kevin-s93@hotmail.com>
2018-07-27 00:11:25 -0300

    Basic js logic for adding a tweet to a list

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 const textArea = document.querySelector('#tweet');
+const tweetList = document.getElementById('tweetList');
 
-function showText() {
+function showText(e) {
+   e.preventDefault();
    let tweet = textArea.value;
+   let li = document.createElement('li');
+
+   li.textContent = tweet;
+   li.classList.add('list-group-item');
+   tweetList.appendChild(li);
 
    console.log(tweet);
 }
